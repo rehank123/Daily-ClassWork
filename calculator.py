89bc4d03eb5869ac17ffc3c7e06b65067a3274c9
rehank123 <157531836+rehank123@users.noreply.github.com>
2024-01-24 08:30:32 -0800

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,21 @@
+a=7
+b=5
+ans1= a + b
+
+print ("Answer of",a,"+",b,"is:",ans1)
+
+ans2= a-b
+print ("Answer of",a,"-",b,"is:",ans2)
+
+ans3= a*b
+
+print ("Answer of",a,"×",b,"is:",ans3)
+
+ans4=a/b
+print ("Answer of",a,"÷",b,"is:",ans4)
+
+ans5=a%b
+print ("Answer of",a,"%",b,"is:",ans5)
+
+ans6=a//b
+print ("Answer of",a,"//",b,"is:",ans6)
