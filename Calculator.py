430031f864122c8fc123b6fcc30dc41ad1294d6b
rehank123 <157531836+rehank123@users.noreply.github.com>
2024-01-24 08:20:58 -0800

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,21 @@
-a = 20
-b = 30
-c= a+b
-print(c)
+a=7
+b=5
+ans1= a + b
+
+print ("Answer of",a,"+",b,"is:",ans1)
+
+ans2= a-b
+print ("Answer of",a,"-",b,"is:",ans2)
+
+ans3= a*b
+
+print ("Answer of",a,"×",b,"is:",ans3)
+
+ans4=a/b
+print ("Answer of",a,"÷",b,"is:",ans4)
+
+ans5=a%b
+print ("Answer of",a,"%",b,"is:",ans5)
+
+ans6=a//b
+print ("Answer of",a,"//",b,"is:",ans6)
