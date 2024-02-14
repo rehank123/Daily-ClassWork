962e0791b621050a2305fb6cab1ffb1bd1ec03b9
Rehan Ullah <157531836+rehank123@users.noreply.github.com>
2024-02-14 07:56:47 -0800

    Update classexample.py

--- classexample.py
+++ classexample.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.title("Welcome Bano Qabil")
 st.write("Pythan CIT  by Sir Ghufran Kamaluddin")
 st.title("Campus: PCHMC")
-st.title("Timing 8 to 10")
+st.write("Timing 8 to 10")
