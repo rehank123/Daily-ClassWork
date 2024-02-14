5b711b05bfc3ed435e9708db243ca1984f2fc458
Rehan Ullah <157531836+rehank123@users.noreply.github.com>
2024-02-14 08:23:58 -0800

    Update classexampletable.py

--- classexampletable.py
+++ classexampletable.py
@@ -5,7 +5,7 @@ st.title("Welcome Bano Qabil")
 
 #creating dataset
 
-df=pd.DataFrame({'first':[1,12,3,54,55,],'second':[66,7,8,9,10]})
+df=pd.DataFrame({'first':[1,12,13,54,55,],'second':[66,7,8,9,10]})
 st.write(df)
 
 # or
