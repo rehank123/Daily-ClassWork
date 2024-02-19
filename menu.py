2c230a7350161f80e89dd75512f209717c63d663
Rehan Ullah <157531836+rehank123@users.noreply.github.com>
2024-02-19 08:02:42 -0800

    Update menu.py

--- menu.py
+++ menu.py
@@ -2,6 +2,5 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 with st.sidebar:
-    selected = option_menu("Main Menu", ["Home", 'Settings'], 
-        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected = option_menu("Main Menu", ["Home", "Settings"], icons=['house', 'gear'], default_index=1)
     selected
