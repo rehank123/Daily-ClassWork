8c9576970b31e6aa9da5c50e22b2cad50f1ab0b3
Rehan Ullah <157531836+rehank123@users.noreply.github.com>
2024-02-19 08:00:42 -0800

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,14 +1,7 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
-def option_menu(title, options, icons, default_index):
-    selected_option = st.sidebar.radio(
-        title,
-        options=options,
-        format_func=lambda x: f'<span style="vertical-align:middle;"><i class="material-icons">{icons[options.index(x)]}</i></span> {x}',
-        index=default_index
-    )
-    return selected_option
-
-if __name__ == "__main__":
-    selected = option_menu("Main Menu", ["Home", "Settings"], icons=['house', 'gear'], default_index=1)
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Settings'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
     selected
