f9113575c42d62c04278cf79950c6710c76158cc
Rehan Ullah <157531836+rehank123@users.noreply.github.com>
2024-02-19 08:38:31 -0800

    Create Add Record.py

---
+++ Add Record.py
@@ -0,0 +1,58 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:/Users/Ghuffi/Desktop/ghufran/input.py ==============
+No students registered yet.
+
+Enter an option: 
+1. Add a student
+2. View a student's data
+3. Exit the program
+
+1
+Enter the student's name: umer
+Enter the student's father's name: farooq
+Enter the student's age: 40
+There is no classes available for entered age.
+Press Enter to continue....
+No students registered yet.
+
+Enter an option: 
+1. Add a student
+2. View a student's data
+3. Exit the program
+
+1
+Enter the student's name: asif
+Enter the student's father's name: khan
+Enter the student's age: 15
+9. Grade 9 (seats available: 20)
+10. Grade 10 (seats available: 20)
+Enter the class number: 9
+Student asif has been added to Class: 9!, with the GR number: 1
+Press Enter to continue....
+
+We currently have 1 registered students in our application
+
+Enter an option: 
+1. Add a student
+2. View a student's data
+3. Exit the program
+
+2
+Enter the GR number of the student: 1
+Student name: asif
+Student father's name: khan
+Student age: 15
+Student class: 9
+Press Enter to continue....
+
+We currently have 1 registered students in our application
+
+Enter an option: 
+1. Add a student
+2. View a student's data
+3. Exit the program
+
+3
+Goodbye!
